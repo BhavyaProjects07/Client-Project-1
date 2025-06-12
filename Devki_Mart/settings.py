b28ee44eb7f08d7ea3c69e73cdac29d51c19affb
BhavyaProjects07 <laptopuse01824x@gmail.com>
2025-06-12 21:41:22 +0530

    Update project

--- Devki_Mart/settings.py
+++ Devki_Mart/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config("SECRET_KEY")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.1']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # Installed apps
 INSTALLED_APPS = [
